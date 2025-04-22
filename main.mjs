b9732f9a0a232c77b15bc3a61efb321821324063
Serhii Tustanovskyi <serhii@MacBook-Air-Serhii.local>
2025-04-22 14:30:59 +0200

    feat: format number

--- main.mjs
+++ main.mjs
@@ -1,71 +1,89 @@
-export const formatToString = n => `${n}`
+const DIGIT_GRADE_REGEX = /(\d+)(\d{3})/;
 
-export const trimLastZeroes = value => {
-  if (value.indexOf('.') === -1) return value
-  return value.replace(/\.?0+$/, '')
-}
+export const formatToString = (n) => `${n}`;
 
-export const formatFixed = (n, p = 2) => n.toFixed(p)
+export const addSpace = (value) => {
+  const [integer, fraction] = value.split('.')
+  const formattedInteger = integer.replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+  return fraction ? `${formattedInteger}.${fraction}` : formattedInteger
+};
+
+
+export const trimLastZeroes = (value) => {
+  if (value.indexOf('.') === -1) return value;
+  return value.replace(/\.?0+$/, '');
+};
+
+export const formatFixed = (n, p = 2) => n.toFixed(p);
 
 /*
  * @precision: -2, -1, 0, 1, 2, 3, 4, 5
  * @return: '123456.78'
  */
-export const formatByPrecision = (n, { precision, zeroEnd, spase }) => {
-  const factor = Math.pow(10, Math.abs(precision))
-  const isAboveZero =  precision > 0
-  const scaled = isAboveZero
-  ? Math.floor(n * factor) / factor
-  : precision === 0 ? (n / factor) * factor : (n  * factor)
-  
-  const digits = isAboveZero ? precision : 1
-  return scaled.toFixed(digits)
-  
-}
+export const formatByPrecision = (n, { precision, zeroEnd = false, spase = false }) => {
+  const factor = Math.pow(10, Math.abs(precision));
+  const isAboveZero = precision > 0;
+  let scaled = isAboveZero
+    ? Math.floor(n * factor) / factor
+    : precision === 0
+    ? (n / factor) * factor
+    : n * factor;
+
+  const digits = isAboveZero ? precision : 1;
+
+  scaled = formatFixed(scaled, digits);
+
+  if (zeroEnd) {
+    scaled = trimLastZeroes(scaled);
+  } else if (spase) {
+    scaled = addSpace(scaled);
+  }
+  return scaled;
+};
 /*
  * @precision: -2, -1, 0, 1, 2, 3, 4, 5
  * @return: '123456.7800'
  */
-export const formatByPrecisionWithZeroEnd = (n, precision) => `${n}`
+export const formatByPrecisionWithZeroEnd = (n, precision) => `${n}`;
 /*
  * @precision: -2, -1, 0, 1, 2, 3, 4, 5
  * @return: '123 456.78'
  */
-export const formatByPrecisionWithSpace = (n, precision) => `${n}`
+export const formatByPrecisionWithSpace = (n, precision) => `${n}`;
 /*
  * @precision: -2, -1, 0, 1, 2, 3, 4, 5
  * @return: '123 456.7800'
  */
-export const formatByPrecisionWithSpaceZeroEnd = (n, precision) => `${n}`
+export const formatByPrecisionWithSpaceZeroEnd = (n, precision) => `${n}`;
 
 /*
  * @step: 100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001
  * @return: '123456.78'
  */
 export const formatByStep = (n, { step, zeroEnd, spase }) => {
-  const stepStr = step.toString()
+  const stepStr = step.toString();
   const mapper = {
     '1e-8': 8,
-    '1e-7': 7
-  }
-  const isInteger = Number.isInteger(step)
-  let decimals = !isInteger ? mapper[stepStr] ?? stepStr.split(".")[1].length : step
-  const res = isInteger ? n * decimals : n / Math.pow(10, decimals)
-  const fixed = isInteger ? 1 : decimals
-  return res.toFixed(fixed)
-}
+    '1e-7': 7,
+  };
+  const isInteger = Number.isInteger(step);
+  let decimals = !isInteger ? mapper[stepStr] ?? stepStr.split('.')[1].length : step;
+  const res = isInteger ? n * decimals : n / Math.pow(10, decimals);
+  const fixed = isInteger ? 1 : decimals;
+  return res.toFixed(fixed);
+};
 /*
  * @step: 100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001
  * @return: '123456.7800'
  */
-export const formatByStepWithZeroEnd = (n, step) => `${n}`
+export const formatByStepWithZeroEnd = (n, step) => `${n}`;
 /*
  * @step: 100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001
  * @return: '123 456.78'
  */
-export const formatByStepWithSpace = (n, step) => `${n}`
+export const formatByStepWithSpace = (n, step) => `${n}`;
 /*
  * @step: 100, 10, 1, 0.1, 0.01, 0.001, 0.0001, 0.00001, 0.000001, 0.0000001
  * @return: '123 456.7800'
  */
-export const formatByStepWithSpaceZeroEnd = (n, step) => `${n}`
+export const formatByStepWithSpaceZeroEnd = (n, step) => `${n}`;
--- main.test.mjs
+++ main.test.mjs
@@ -1,31 +1,49 @@
 import test from 'node:test'
 import { strictEqual, ok } from 'node:assert';
 
-import { formatByPrecision, formatByStep } from './main.mjs'
+import { formatByPrecision, formatByStep, addSpace } from './main.mjs'
 
 test('Price formating', { concurrency: true }, async (t) => {
-  // await t.test('formatByPrecision', (t) => {
-  //   const testCases = [
-  //     { price: 0.000123456, precision: 8, expected: '0.00012345' },
-  //     { price: 0.00123456, precision: 7, expected: '0.0012345' },
-  //     { price: 0.0123456, precision: 6, expected: '0.012345' },
-  //     { price: 0.123456, precision: 5, expected: '0.12345' },
-  //     { price: 1.23456, precision: 4, expected: '1.2345' },
-  //     { price: 12.3456, precision: 3, expected: '12.345' },
-  //     { price: 123.456, precision: 2, expected: '123.45' },
-  //     { price: 1234.56, precision: 1, expected: '1234.5' },
-  //     { price: 12345.6, precision: 0, expected: '12345.6' },
-  //     { price: 123456.0, precision: -1, expected: '1234560.0' },
-  //     { price: 1234560.0, precision: -2, expected: '123456000.0' },
-  //   ]
+  await t.test('formatByPrecision', (t) => {
+    const testCases = [
+      { price: 0.000123456, precision: 8, expected: '0.00012345' },
+      { price: 0.00123456, precision: 7, expected: '0.0012345' },
+      { price: 0.0123456, precision: 6, expected: '0.012345' },
+      { price: 0.123456, precision: 5, expected: '0.12345' },
+      { price: 1.23456, precision: 4, expected: '1.2345' },
+      { price: 12.3456, precision: 3, expected: '12.345' },
+      { price: 123.456, precision: 2, expected: '123.45' },
+      { price: 1234.56, precision: 1, expected: '1234.5' },
+      { price: 12345.6, precision: 0, expected: '12345.6' },
+      { price: 123456.0, precision: -1, expected: '1234560.0' },
+      { price: 1234560.0, precision: -2, expected: '123456000.0' },
+      { price: 1234560.0, precision: -2, expected: '123456000.0' },
+    ]
 
-  //   testCases.forEach(({ price, precision, expected }) => {
-  //     const actual = formatByPrecision(price, {precision})
-  //     const message = `expected: ${expected}, actual: ${actual}, price: ${price}, precision: ${precision})`
+    testCases.forEach(({ price, precision, expected }) => {
+      const actual = formatByPrecision(price, {precision})
+      const message = `expected: ${expected}, actual: ${actual}, price: ${price}, precision: ${precision})`
 
-  //     strictEqual(actual, expected, message)
-  //   })
-  // })
+      strictEqual(actual, expected, message)
+    })
+  })
+
+  await t.test('formatNumber', (t) => {
+    const testCases = [
+      { value: '0', expected: '0' },
+      { value: '1000', expected: '1 000' },
+      { value: '123456789', expected: '123 456 789' },
+      { value: '123456789.123456789', expected: '123 456 789.123456789' },
+  
+    ]
+
+    testCases.forEach(({ value, expected }) => {
+      const actual = addSpace(value)
+      const message = `expected: ${expected}, actual: ${actual}, value: ${value}`
+
+      strictEqual(actual, expected, message)
+    })
+  })
 
   await t.test('formatByStep 1', (t) => {
     const testCases = [
